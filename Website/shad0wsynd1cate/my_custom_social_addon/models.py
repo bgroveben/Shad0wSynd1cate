fc521dcc15956498f7b9f8ee6e17e2266f734887
bgroveben <bgroveben@gmail.com>
2017-12-26 02:50:20 -0800

    Add the basic models for the social media plugin

--- Website/shad0wsynd1cate/my_custom_social_addon/models.py
+++ Website/shad0wsynd1cate/my_custom_social_addon/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+
+from cms.models import CMSPlugin
 
 # Create your models here.
+
+@python_2_unicode_compatible
+class Social(CMSPlugin):
+    label = models.CahrField(
+        blank=True,
+        max_length=200,
+    )
+
+    def __str__(self):
+        return self.label

df9578993190c5dc4ac604747c1d1da701d4c015
bgroveben <bgroveben@gmail.com>
2018-01-03 05:35:55 -0800

    Remove tutorial website and prototypes

--- Website/site_venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/benjamingrove/Desktop/Shad0wSynd1cate/Website/site_venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
